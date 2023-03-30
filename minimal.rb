b96928445f511b2e42adea854f800cd3990d9b4d
Oscar Welton <oscarwelton@gmail.com>
2023-03-30 21:52:50 +0200

    edit: removed bootstrap and other bloat

--- minimal.rb
+++ minimal.rb
@@ -23,9 +23,6 @@ gsub_file("Gemfile", '# gem "sassc-rails"', 'gem "sassc-rails"')
 ########################################
 run "rm -rf app/assets/stylesheets"
 run "rm -rf vendor"
-run "curl -L https://github.com/lewagon/rails-stylesheets/archive/master.zip > stylesheets.zip"
-run "unzip stylesheets.zip -d app/assets && rm -f stylesheets.zip && rm -f app/assets/rails-stylesheets-master/README.md"
-run "mv app/assets/rails-stylesheets-master app/assets/stylesheets"
 
 inject_into_file "config/initializers/assets.rb", before: "# Precompile additional assets." do
   <<~RUBY
@@ -41,13 +38,6 @@ gsub_file(
   '<meta name="viewport" content="width=device-width,initial-scale=1">',
   '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
 )
-
-# README
-########################################
-markdown_file_content = <<~MARKDOWN
-  Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
-MARKDOWN
-file "README.md", markdown_file_content, force: true
 
 # Generators
 ########################################
@@ -68,7 +58,7 @@ after_bundle do
   # Generators: db + simple form + pages controller
   ########################################
   rails_command "db:drop db:create db:migrate"
-  generate("simple_form:install", "--bootstrap")
+  generate("simple_form:install")
   generate(:controller, "pages", "home", "--skip-routes", "--no-test-framework")
 
   # Routes
@@ -86,13 +76,6 @@ after_bundle do
     .DS_Store
   TXT
 
-  # Yarn
-  ########################################
-  run "yarn add bootstrap @popperjs/core"
-  append_file "app/javascript/application.js", <<~JS
-    import "bootstrap"
-  JS
-
   # Heroku
   run "bundle lock --add-platform x86_64-linux"
 
@@ -100,13 +83,4 @@ after_bundle do
   ########################################
   run "touch '.env'"
 
-  # Rubocop
-  ########################################
-  run "curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml"
-
-  # Git
-  ########################################
-  git :init
-  git add: "."
-  git commit: "-m 'Initial commit with minimal template from https://github.com/lewagon/rails-templates'"
 end
